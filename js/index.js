e3bc862269c43aaea6fa5b0a70d6f449d2afba8c
ifiokudoidiok <ifiok_udoidiok@yahoo.com>
2019-09-03 16:13:09 +0100

    Added scroll event in JS file

--- js/index.js
+++ js/index.js
@@ -98,3 +98,17 @@ window.addEventListener('load', (event) => {
   window.addEventListener('resize', (event) => {
     console.log('page is being resized!');
   });
+
+  //scroll
+  let last_known_scroll_position = 0;
+let ticking = false;
+
+function doSomething(scroll_pos) {
+  // Do something with the scroll position
+}
+
+window.addEventListener('scroll', function(e) {
+  last_known_scroll_position = window.scrollY;
+
+  console.log(last_known_scroll_position);
+});
