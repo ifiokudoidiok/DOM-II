2f2842bf6f2f34871100841cd8a6a9fc27db79fb
ifiokudoidiok <ifiok_udoidiok@yahoo.com>
2019-09-03 13:12:29 +0100

    Added Mouseover event in JS file

--- js/index.js
+++ js/index.js
@@ -1,1 +1,19 @@
 // Your code goes here
+
+//mouseover
+let navEvent = document.querySelector(".nav");
+
+
+// This handler will be executed every time the cursor
+// is moved over a different list item
+navEvent.addEventListener("mouseover", ( event ) =>{   
+  // highlight the mouseover target
+  event.target.style.color = "orange";
+
+  // reset the color after a short delay
+  setTimeout(() => {
+    event.target.style.color = "";
+  }, 500);
+}, false);
+
+//keydown
